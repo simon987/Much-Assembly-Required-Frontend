19897e008070a0fb7662be62c3fd1b35b92129c8
Simon Fortier <simon987@users.noreply.github.com>
2017-11-22 19:27:13 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -7,13 +7,3 @@ create table mar_user
   tokenTime datetime null,
   floppyData mediumblob null
 );
-
-create table mar_userdata
-(
-  username varchar(20) not null
-    primary key,
-  userCode text null,
-  processed tinyint default '0' not null,
-  constraint fk_mar_userdata_mar_user1
-  foreign key (username) references mar_user (username)
-);
